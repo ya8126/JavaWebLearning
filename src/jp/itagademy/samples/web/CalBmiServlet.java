4c355e74140f487c54c325d777bec569cb890d1f
ya8126 <yamamoto8836@hotmail.com>
2012-08-18 05:18:24 +0900

    8/18 5:18

---
+++ src/jp/itagademy/samples/web/CalBmiServlet.java
@@ -0,0 +1,47 @@
+package jp.itagademy.samples.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowParametersServlet
+ */
+@WebServlet("/bmi")
+public class CalBmiServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+
+		out.println("<!DOCTYPE html>");
+		out.println("<meta charset=\"utf-8\">");
+		out.println("<title>BMI計算</title>");
+		out.println("<link rel=\"stylesheet\" href=\"bmi.css\">");
+		out.println("<h1>計算結果</h1>");
+		out.println("<p>あなたのBMIは");
+		double height = Double.parseDouble(request.getParameter("height"));
+		double weight = Double.parseDouble(request.getParameter("weight"));
+		double bmi = weight * 10000 / height / height;
+		out.println("<span class=\"bmi\">" + bmi + "</span>");
+		out.println("です。");
+	}
+
+}
